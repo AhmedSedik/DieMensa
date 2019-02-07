9d588f4b89f7a15a483352b8e60b3c7823446895
Ahmad <ahmedsedeek2007@gmail.com>
2019-02-08 00:51:12 +0100

    Extra

---
+++ app/src/main/java/com/example/ahmed/diemensa/Like.java
@@ -0,0 +1,23 @@
+package com.example.ahmed.diemensa;
+
+public class Like {
+
+
+    public String likes;
+
+
+
+    public Like(){
+
+    }
+    public Like(String likes){
+
+        this.likes = likes;
+
+    }
+
+    public String getLikes() {
+        return likes;
+    }
+
+}
